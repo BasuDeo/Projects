62a876334dc30509906dfbfb69f6ebb771311e10
Virender Singh <virender502bora@gmail.com>
2018-10-15 16:10:05 +0530

    SpringBoot App initial commit

---
+++ CEOApp/src/main/java/com/basu/CEOApp/CeoAppApplication.java
@@ -0,0 +1,12 @@
+package com.basu.CEOApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeoAppApplication.class, args);
+	}
+}
---
+++ CEOApp/src/test/java/com/basu/CEOApp/CeoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.basu.CEOApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CeoAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
